e8c02339d7c9dfa48bd8f2904e612e8a08162893
Michał Rus <m@michalrus.com>
2014-01-25 17:25:28 +0000

    Ignore non-Unit statements in desugaring of XML literals

--- src/main/scala/wartremover/warts/NonUnitStatements.scala
+++ src/main/scala/wartremover/warts/NonUnitStatements.scala
@@ -4,13 +4,19 @@ package warts
 object NonUnitStatements extends WartTraverser {
   def apply(u: WartUniverse): u.Traverser = {
     import u.universe._
+    import scala.reflect.NameTransformer
 
     val PrintName: TermName = "$print"
+    val NodeBufferAddName: TermName = NameTransformer.encode("&+")
 
     def isIgnoredStatement(tree: Tree) = tree match {
       // Scala creates synthetic blocks with <init> calls on classes.
       // The calls return Object so we need to ignore them.
       case Apply(Select(_, nme.CONSTRUCTOR), _) => true
+      // scala.xml.NodeBuffer#&+ returns NodeBuffer instead of Unit, so
+      // val x = <x>5</x> desugars to a non-Unit statement; ignore.
+      case Apply(Select(qual, NodeBufferAddName), _)
+        if qual.symbol.typeSignature =:= typeOf[scala.xml.NodeBuffer] => true
       case _ => false
     }
 
