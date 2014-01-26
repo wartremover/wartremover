a272e7c43e12c7fcaab1ff454b7cefd6e79bfddb
Brian McKenna <brian@brianmckenna.org>
2014-01-25 21:31:17 -0700

    Get case classes working again
    Fixes #35

--- src/main/scala/wartremover/warts/ForbidInference.scala
+++ src/main/scala/wartremover/warts/ForbidInference.scala
@@ -8,13 +8,17 @@ trait ForbidInference[T] extends WartTraverser {
     val tSymbol = typeOf[T].typeSymbol
     new Traverser {
       override def traverse(tree: Tree) {
+        val synthetic = isSynthetic(u)(tree)
         def error() = u.error(tree.pos, s"Inferred type containing ${tSymbol.name}")
+
         tree match {
           case tpt @ TypeTree() if wasInferred(u)(tpt) && tpt.tpe.contains(tSymbol) =>
             error()
+          case ValDef(_, _, tpt: TypeTree, _) if wasInferred(u)(tpt) && tpt.tpe.contains(tSymbol) && synthetic =>
+          case DefDef(_, _, _, _, tpt: TypeTree, _) if wasInferred(u)(tpt) && tpt.tpe.contains(tSymbol) && synthetic =>
           case _ =>
+            super.traverse(tree)
         }
-        super.traverse(tree)
       }
     }
   }
