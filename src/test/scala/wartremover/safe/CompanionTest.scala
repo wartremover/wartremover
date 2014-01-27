579d1a0de9f1aaa45b896e7dd362320af22c988f
Michał Rus <m@michalrus.com>
2014-01-27 07:07:55 +0000

    Test companion-to-a-case-class against all warts
    Currently Serializable catches `object A; case class A(a: Int)`,
    see issue #37.

--- src/test/scala/wartremover/safe/CompanionTest.scala
+++ src/test/scala/wartremover/safe/CompanionTest.scala
@@ -3,11 +3,11 @@ package test
 
 import org.scalatest.FunSuite
 
-import org.brianmckenna.wartremover.warts.Null
+import org.brianmckenna.wartremover.warts.Unsafe
 
 class CompanionTest extends FunSuite {
   test("can use companion objects for case classes") {
-    val result = WartTestTraverser(Null) {
+    val result = WartTestTraverser(Unsafe) {
       case class Foo(n: Int)
       object Foo {
       }
