44ed07e892e6365c1ac60fe0a3a41cf114b06ed9
Peter Hausel <peter_kovac11@hotmail.com>
2014-03-10 18:04:40 -0400

    add new JavaConversions and MutableDataStructure WartTraversers - JavaConversions: disabling scala.collection.JavaConversions - MutableDataStructure: disabling scala.collection.mutable._

---
+++ src/main/scala/wartremover/warts/JavaConversions.scala
@@ -0,0 +1,22 @@
+package org.brianmckenna.wartremover
+package warts
+
+object JavaConversions extends WartTraverser {
+  def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+
+    val javaConversions = rootMirror.staticModule("scala.collection.JavaConversions")
+
+    new Traverser {
+      override def traverse(tree: Tree) {
+        tree match {
+          case Select(tpt, _) if tpt.tpe.contains(javaConversions) => {
+            u.error(tree.pos, "scala.collection.JavaConversions is disabled - use scala.collection.JavaConverters instead")
+          }
+          case _ =>
+        }
+        super.traverse(tree)
+      }
+    }
+  }
+}
---
+++ src/test/scala/wartremover/warts/MutableDataStructuresTest.scala
@@ -0,0 +1,23 @@
+package org.brianmckenna.wartremover
+package test
+
+import org.scalatest.FunSuite
+
+import org.brianmckenna.wartremover.warts.MutableDataStructures
+
+class MutableDataStructuresTest extends FunSuite {
+  test("disable scala.collection.mutable._ when referenced") {
+    val result = WartTestTraverser(MutableDataStructures) {
+      var x = scala.collection.mutable.HashMap("key" -> "value")
+    }
+    expectResult(List("scala.collection.mutable package is disabled -  use java.util instead"), "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+  test("ignore immutable collections") {
+    val result = WartTestTraverser(MutableDataStructures) {
+      var x = Map("key" -> "value")
+    }
+    expectResult(List.empty, "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+}
