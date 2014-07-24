020ae34ccff8fec2f7d6f35a55dfb2fb150cd708
Jonathan Ferguson <jono@spiralarm.com>
2014-07-24 14:06:36 +1000

    Added warts for List head, tail and last.

---
+++ src/test/scala/wartremover/warts/ListTest.scala
@@ -0,0 +1,33 @@
+package org.brianmckenna.wartremover
+package test
+
+import org.scalatest.FunSuite
+
+import org.brianmckenna.wartremover.warts.ListOps._
+
+class ListTest extends FunSuite {
+  test("can't use List#head on List") {
+    val result = WartTestTraverser(Head) {
+      println(List(1).head)
+    }
+    expectResult(List("List#head is disabled - use List#headOption instead"), "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("can't use List#tail on  List") {
+    val result = WartTestTraverser(Tail) {
+      println(List().tail)
+    }
+    expectResult(List("List#tail is disabled - use List#drop(1) instead"), "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("can't use List#last on  List") {
+    val result = WartTestTraverser(Last) {
+      println(List().last)
+    }
+    expectResult(List("List#last is disabled - use List#lastOption instead"), "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+}
