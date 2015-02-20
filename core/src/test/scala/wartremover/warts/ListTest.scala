73dc8a5c049e4b7c6e92f6501032a2e4ae262661
Cody Allen <ceedubs@gmail.com>
2015-02-20 07:38:01 -0500

    Add List.reduce* and List.init warts
    Fixes #137

--- core/src/test/scala/wartremover/warts/ListTest.scala
+++ core/src/test/scala/wartremover/warts/ListTest.scala
@@ -22,11 +22,43 @@ class ListTest extends FunSuite {
     expectResult(List.empty, "result.warnings")(result.warnings)
   }
 
+  test("can't use List#init on List") {
+    val result = WartTestTraverser(ListOps) {
+      println(List().init)
+    }
+    expectResult(List("List#init is disabled - use List#dropRight(1) instead"), "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+
   test("can't use List#last on List") {
     val result = WartTestTraverser(ListOps) {
       println(List().last)
     }
     expectResult(List("List#last is disabled - use List#lastOption instead"), "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("can't use List#reduce on List") {
+    val result = WartTestTraverser(ListOps) {
+      println(List.empty[Int].reduce(_ + _))
+    }
+    expectResult(List("List#reduce is disabled - use List#reduceOption or List#fold instead"), "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("can't use List#reduceLeft on List") {
+    val result = WartTestTraverser(ListOps) {
+      println(List.empty[Int].reduceLeft(_ + _))
+    }
+    expectResult(List("List#reduceLeft is disabled - use List#reduceLeftOption or List#foldLeft instead"), "result.errors")(result.errors)
+    expectResult(List.empty, "result.warnings")(result.warnings)
+  }
+
+  test("can't use List#reduceRight on List") {
+    val result = WartTestTraverser(ListOps) {
+      println(List.empty[Int].reduceRight(_ + _))
+    }
+    expectResult(List("List#reduceRight is disabled - use List#reduceRightOption or List#foldRight instead"), "result.errors")(result.errors)
     expectResult(List.empty, "result.warnings")(result.warnings)
   }
 
