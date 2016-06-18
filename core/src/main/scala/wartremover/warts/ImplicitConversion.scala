3a2c707a7c170f4799242f2bcb5541febe39967d
tim-zh <tim.zhlobich@gmail.com>
2016-06-18 19:48:52 +0300

    Add ImplicitConversion wart

---
+++ core/src/main/scala/wartremover/warts/ImplicitConversion.scala
@@ -0,0 +1,22 @@
+package org.brianmckenna.wartremover
+package warts
+
+object ImplicitConversion extends WartTraverser {
+  def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+
+    new u.Traverser {
+      override def traverse(tree: Tree): Unit = {
+        tree match {
+          // Ignore trees marked by SuppressWarnings
+          case t if hasWartAnnotation(u)(t) =>
+          case t: DefDef if t.symbol.isImplicit && t.symbol.isPublic && !isSynthetic(u)(t) =>
+            u.error(tree.pos, "Implicit conversion is disabled")
+            super.traverse(tree)
+          case _ =>
+            super.traverse(tree)
+        }
+      }
+    }
+  }
+}
