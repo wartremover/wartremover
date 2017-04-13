c8079a52fa0a8c6f383d6529552d38ad711f7d44
Greg Pfeil <greg@technomadic.org>
2017-04-13 00:37:45 +0000

    Recursion wart. (#346)
    * Recursion wart.
    
    * Add SuppressWarnings test to Recursion
    
    * Add documentation for the Recursion wart.

---
+++ core/src/main/scala/wartremover/warts/Recursion.scala
@@ -0,0 +1,27 @@
+package org.wartremover
+package warts
+
+object Recursion extends WartTraverser {
+  def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+
+    new u.Traverser {
+      val Tailrec = typeOf[scala.annotation.tailrec]
+      var open: Set[Symbol] = Set()
+      def inside[A](sym: Symbol)(expr: => A): A = {
+        open = open + sym
+        try expr finally open = open - sym
+      }
+      def isTailrec(m: Symbol) = m.annotations exists (_.tpe <:< Tailrec)
+      override def traverse(tree: Tree): Unit = {
+        tree match {
+          // Ignore trees marked by SuppressWarnings
+          case t if hasWartAnnotation(u)(t)      =>
+          case t: DefDef if !isTailrec(t.symbol) => inside(tree.symbol)(super.traverse(tree))
+          case t: RefTree if open(tree.symbol)   => error(u)(t.pos, "Unmarked recursion") ; super.traverse(tree)
+          case _                                 => super.traverse(tree)
+        }
+      }
+    }
+  }
+}
---
+++ core/src/test/scala/wartremover/warts/RecursionTest.scala
@@ -0,0 +1,48 @@
+package org.wartremover
+package test
+
+import org.scalatest.FunSuite
+import scala.annotation.tailrec
+import org.wartremover.warts.Recursion
+
+class RecursionTest extends FunSuite with ResultAssertions {
+
+  test("can't use recursion") {
+    val result = WartTestTraverser(Recursion) {
+      def foo(x: Int): Int = foo(x)
+    }
+    assertError(result)("Unmarked recursion")
+  }
+
+  test("can't use nested recursion") {
+    val result = WartTestTraverser(Recursion) {
+      def foo(x: Int): Int = {
+        def bar(y: Int): Int = foo(y)
+        bar(1)
+      }
+    }
+    assertError(result)("Unmarked recursion")
+  }
+
+  test("can use non-recursion") {
+    val result = WartTestTraverser(Recursion) {
+      def foo(x: Int): Int = x
+    }
+    assertEmpty(result)
+  }
+
+  test("can use tail recursion") {
+    val result = WartTestTraverser(Recursion) {
+      @tailrec def foo(x: Int): Int = if (x < 0) 1 else foo(x - 1)
+    }
+    assertEmpty(result)
+  }
+
+  test("Recursion wart obeys SuppressWarnings") {
+    val result = WartTestTraverser(Recursion) {
+      @SuppressWarnings(Array("org.wartremover.warts.Recursion"))
+      def foo(x: Int): Int = foo(x)
+    }
+    assertEmpty(result)
+  }
+}
