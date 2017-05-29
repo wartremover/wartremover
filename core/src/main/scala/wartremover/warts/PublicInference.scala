cd70b20005606ac11db0f325fa3aa8de8319b308
Tim Zhlobich <tim.zhlobich@gmail.com>
2017-05-29 12:37:19 +0300

    PublicInference partial function test (#356)

--- core/src/main/scala/wartremover/warts/PublicInference.scala
+++ core/src/main/scala/wartremover/warts/PublicInference.scala
@@ -4,7 +4,13 @@ package warts
 object PublicInference extends WartTraverser {
   def apply(u: WartUniverse): u.Traverser = {
     import u.universe._
-    import u.universe.Flag._
+
+    def isInPublicClass(t: Tree) = t.symbol.owner.isClass && t.symbol.owner.isPublic
+
+    def isConstructorOrOverrides(t: Tree) = t.symbol.isMethod && (t.symbol.asMethod.isConstructor ||
+        t.symbol.asMethod.allOverriddenSymbols.nonEmpty)
+
+    def isAccessor(t: Tree) = t.symbol.isTerm && (t.symbol.asTerm.isParamAccessor || t.symbol.asTerm.isCaseAccessor)
 
     new u.Traverser {
       override def traverse(tree: Tree): Unit = {
@@ -12,12 +18,8 @@ object PublicInference extends WartTraverser {
           // Ignore trees marked by SuppressWarnings
           case t if hasWartAnnotation(u)(t) =>
 
-          case t: ValOrDefDef if isPublic(u)(t) &&
-              t.symbol.owner.isClass && t.symbol.owner.isPublic &&
-              !(t.symbol.isMethod && (t.symbol.asMethod.isConstructor ||
-                  t.symbol.asMethod.allOverriddenSymbols.nonEmpty)) &&
-              !(t.symbol.isTerm && (t.symbol.asTerm.isParamAccessor ||
-                  t.symbol.asTerm.isCaseAccessor)) &&
+          case t: ValOrDefDef if isPublic(u)(t) && isInPublicClass(t) &&
+              !isConstructorOrOverrides(t) && !isAccessor(t) &&
               !hasTypeAscription(u)(t) && !isSynthetic(u)(tree) =>
             error(u)(tree.pos, "Public member must have an explicit type ascription")
 
