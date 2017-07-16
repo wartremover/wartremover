2e266b6ac9ecae67ebfdef1d45ae88636ebd5d52
nigredo-tori <nigredo-tori@users.noreply.github.com>
2017-07-17 02:02:50 +0700

    Make IsInstanceOf ignore macro expansions (#370)
    * Disable `IsInstanceOf` for macro expansions
    
    * Add a test macro to check `IsInstanceOf` behaviour

---
+++ test-macros/src/main/scala/wartremover/test/IsInstanceOfTestMacros.scala
@@ -0,0 +1,24 @@
+package org.wartremover
+package test
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+
+object IsInstanceOfTestMacros {
+
+  def is[A, B]: A => Boolean = macro IsInstanceOfTestMacrosImpl.is_impl[A, B]
+}
+
+@macrocompat.bundle
+private class IsInstanceOfTestMacrosImpl(val c: blackbox.Context) {
+  def is_impl[A: c.WeakTypeTag, B: c.WeakTypeTag]: c.Expr[A => Boolean] = {
+    import c.universe._
+
+    val aTpe = weakTypeOf[A]
+    val bTpe = weakTypeOf[B]
+
+    c.Expr[A => Boolean](q"""
+      (a: $aTpe) => a.isInstanceOf[$bTpe]
+    """)
+  }
+}
