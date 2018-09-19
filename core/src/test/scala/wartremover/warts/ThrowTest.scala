db9368d4b079684fd60aa626f48903308ceb9798
xuwei-k <6b656e6a69@gmail.com>
2018-09-19 15:00:38 +0900

    add test case for issue 175
    fix https://github.com/wartremover/wartremover/issues/175
    
    see
    - https://github.com/wartremover/wartremover/blob/v2.3.6/core/src/main/scala/wartremover/warts/Throw.scala#L17
    - https://github.com/wartremover/wartremover/commit/4c77baa3edf14d4074cbf1aa824d048f48864e13#diff-740542d5ccd38d2eafc47aa2198a5efe

--- core/src/test/scala/wartremover/warts/ThrowTest.scala
+++ core/src/test/scala/wartremover/warts/ThrowTest.scala
@@ -20,6 +20,16 @@ class ThrowTest extends FunSuite with ResultAssertions {
     assertError(result)("throw is disabled")
   }
 
+  test("throw is allowed in synthetic MatchError") {
+    val result = WartTestTraverser(Throw) {
+      val (a, b) = (1 to 10).partition(_ % 2 == 0)
+      Option(2) match {
+        case Some(1) =>
+      }
+    }
+    assertEmpty(result)
+  }
+
   test("throw is allowed in synthetic Product.productElement") {
     val result = WartTestTraverser(Throw) {
       case class Foo(i: Int)
