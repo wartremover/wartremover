ff013fca7fbeabf527fc7451f8e8df87578716c7
xuwei-k <6b656e6a69@gmail.com>
2022-02-09 08:01:01 +0900

    add RedundantConversions wart

---
+++ core/src/main/scala/wartremover/warts/RedundantConversions.scala
@@ -0,0 +1,38 @@
+package org.wartremover
+package warts
+
+object RedundantConversions extends WartTraverser {
+
+  override def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+    val values: Map[Name, (String, Type)] = Seq(
+      "toList" -> typeOf[List[Any]],
+      "toSeq" -> typeOf[collection.immutable.Seq[Any]],
+      "toVector" -> typeOf[Vector[Any]],
+      "toSet" -> typeOf[Set[Any]],
+      "toStream" -> typeOf[Stream[Any]],
+      "toIndexedSeq" -> typeOf[collection.immutable.IndexedSeq[Any]]
+    ).map { case (name, tpe) =>
+      TermName(name) -> Tuple2(name, tpe)
+    }.toMap
+
+    new Traverser {
+      override def traverse(tree: Tree): Unit = {
+        tree match {
+          case t if hasWartAnnotation(u)(t) =>
+          case Select(obj, TermName("toString")) if obj.tpe <:< typeOf[String] =>
+            error(u)(tree.pos, "redundant toString conversion")
+          case Select(obj, method)  =>
+            values.get(method) match {
+              case Some((name, tpe)) if obj.tpe.typeConstructor <:< tpe.typeConstructor =>
+                error(u)(tree.pos, s"redundant ${name} conversion")
+              case _ =>
+                super.traverse(tree)
+            }
+          case _ =>
+            super.traverse(tree)
+        }
+      }
+    }
+  }
+}
