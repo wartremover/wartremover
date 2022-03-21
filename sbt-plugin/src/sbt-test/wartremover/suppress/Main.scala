4d95cbd95433228fa270916c31f09cbe2c4d3abc
xuwei-k <6b656e6a69@gmail.com>
2022-03-21 10:46:02 +0900

    add more tests

---
+++ sbt-plugin/src/sbt-test/wartremover/suppress/Main.scala
@@ -0,0 +1,82 @@
+package example
+
+import scala.language.implicitConversions
+
+@SuppressWarnings(Array("org.wartremover.warts.All"))
+class Main {
+  final val abc = 4
+
+  def overload(x: Int): String = ""
+  def overload(x: String): String = ""
+
+  def nestMethod1 = {
+    class NestedClass {
+
+      def nestMethod2 = {
+        def x5 = List("x", false) // Any
+        def x1 = List(2, true) // AnyVal
+
+        def x3 = Array(1) == Array(2) // ArrayEquals
+
+        // Null, asInstanceOf
+        null.asInstanceOf[Long]
+
+        def x2: Int = {
+          var i = 0 // Var
+          while (i <= 10) { // While
+            return i // Return
+          }
+          99
+        }
+
+        // ListUnapply, DefaultArguments
+        def x4[B](a: collection.Seq[B] = Nil): Int = a match {
+          case _ :: _ :: _ =>
+            0
+          case _ :: _ =>
+            1
+          case _ =>
+            2
+        }
+
+        // ListAppend
+        List(3) :+ 4
+
+        "str" + Predef
+
+        Nil.head
+
+        null.isInstanceOf[Int] // IsInstanceOf
+
+        scala.util.Try(9).get
+
+        Option(false).get
+
+        Option(""): Iterable[String]
+
+        scala.io.Source.fromFile("foo.txt") // PlatformDefault
+
+        List(9).size == 2 // SizeIs
+
+        Thread.sleep(33) // ThreadSleep
+
+        // DefaultArguments, ImplicitConversion, ImplicitParameter
+        implicit def foo(y: String = "")(implicit p: Long): Option[Int] = None
+
+        println(scala.concurrent.ExecutionContext.global) // GlobalExecutionContext
+
+        println(scala.collection.mutable.ListBuffer[Boolean]()) // MutableDataStructures
+
+        throw new Error("error")
+
+        // EitherProjectionPartial
+        @annotation.nowarn
+        def either = List(
+          Right(3).right.get,
+          Left(2).left.get
+        )
+
+      }
+    }
+  }
+}
---
+++ sbt-plugin/src/sbt-test/wartremover/suppress/build.sbt
@@ -0,0 +1,5 @@
+crossScalaVersions := Seq("2.12.15", "2.13.8")
+
+wartremoverWarnings ++= Warts.all
+
+scalacOptions += "-Xfatal-warnings"
