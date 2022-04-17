80b4f3dc2cd7415ec2a8d10ae40dab87133806a0
xuwei-k <6b656e6a69@gmail.com>
2022-04-18 07:34:45 +0900

    fix `Null` and `Var` false positive for XML
    `Class.forName("xml class name")` does not work with compiler plugin

---
+++ sbt-plugin/src/sbt-test/wartremover/xml/Main.scala
@@ -0,0 +1,8 @@
+package example
+
+object Main {
+  private val x1 = <foo />
+  private val x2 = <a>b</a>
+  private val x3 = <a>{List(2).sum}ccc</a>
+  private val x4 = <a>{List(2).sum}ccc{9}</a>
+}
