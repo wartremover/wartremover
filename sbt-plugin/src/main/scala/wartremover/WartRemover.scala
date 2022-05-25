b55384ebfb7a8614864cf4668431324e5b17a50c
xuwei-k <6b656e6a69@gmail.com>
2022-05-25 11:42:57 +0900

    Scala 3.1.3-RC4

--- sbt-plugin/src/main/scala/wartremover/WartRemover.scala
+++ sbt-plugin/src/main/scala/wartremover/WartRemover.scala
@@ -36,7 +36,7 @@ object WartRemover extends sbt.AutoPlugin {
     Global / concurrentRestrictions += Tags.limit(WartremoverTag, 2),
     wartremoverInspectScalaVersion := {
       // need RC version because there are some bugs in old tasty-inspector.
-      "3.1.3-RC3"
+      "3.1.3-RC4"
     },
     excludeLintKeys += wartremoverInspectOutputFile,
     wartremoverCrossVersion := CrossVersion.full,
