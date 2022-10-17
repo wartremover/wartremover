1f4d10bc7eba9b77f9ae07ba14115af4d9c9e36c
Samuel Nelson <samuel.nelson@elastic.co>
2022-10-17 13:22:01 +1300

    Fmt

--- core/src/main/scala-3/org/wartremover/warts/TripleQuestionMark.scala
+++ core/src/main/scala-3/org/wartremover/warts/TripleQuestionMark.scala
@@ -2,6 +2,6 @@ package org.wartremover
 package warts
 
 object TripleQuestionMark
-  extends ExprMatch({ case '{ scala.Predef.??? } =>
-    "??? is disabled"
-  })
+    extends ExprMatch({ case '{ scala.Predef.??? } =>
+      "??? is disabled"
+    })
