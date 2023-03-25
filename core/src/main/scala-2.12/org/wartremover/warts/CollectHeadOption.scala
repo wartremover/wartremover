3c8fbbbd0b847e4e99bddc5d3a5905dc33ae2a89
xuwei-k <6b656e6a69@gmail.com>
2023-03-25 19:08:21 +0900

    separate CollectHeadOption.scala

---
+++ core/src/main/scala-2.12/org/wartremover/warts/CollectHeadOption.scala
@@ -0,0 +1,25 @@
+package org.wartremover
+package warts
+
+object CollectHeadOption extends WartTraverser {
+
+  def apply(u: WartUniverse): u.Traverser = {
+    import u.universe._
+    val iterable = rootMirror.staticClass("scala.collection.Iterable").toTypeConstructor
+    new u.Traverser {
+      private[this] def message = "you can use collectFirst instead of collect.headOption"
+      override def traverse(tree: Tree): Unit = {
+        tree match {
+          case _ if hasWartAnnotation(u)(tree) =>
+          case Select(
+                Apply(Apply(TypeApply(Select(left, TermName("collect")), _ :: _ :: Nil), pf :: Nil), canBuildFrom),
+                TermName("headOption")
+              ) if left.tpe.typeConstructor <:< iterable =>
+            error(u)(tree.pos, message)
+          case _ =>
+            super.traverse(tree)
+        }
+      }
+    }
+  }
+}
--- core/src/main/scala-2.13/org/wartremover/warts/CollectHeadOption.scala
+++ core/src/main/scala-2.13/org/wartremover/warts/CollectHeadOption.scala
@@ -11,15 +11,8 @@ object CollectHeadOption extends WartTraverser {
       override def traverse(tree: Tree): Unit = {
         tree match {
           case _ if hasWartAnnotation(u)(tree) =>
-          case Select(
-                Apply(Apply(TypeApply(Select(left, TermName("collect")), _ :: _ :: Nil), pf :: Nil), canBuildFrom),
-                TermName("headOption")
-              ) if left.tpe.typeConstructor <:< iterable =>
-            // Scala 2.12
-            error(u)(tree.pos, message)
           case Select(Apply(TypeApply(Select(left, TermName("collect")), _ :: Nil), pf :: Nil), TermName("headOption"))
               if left.tpe.typeConstructor <:< iterable =>
-            // Scala 2.13
             error(u)(tree.pos, message)
           case _ =>
             super.traverse(tree)
