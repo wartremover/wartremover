7671572cdd1fe9803ba4c931e4a94bc81b0d090a
kenji yoshida <6b656e6a69@gmail.com>
2023-04-26 14:44:25 +0900

    Update InspectArgsParserTest.scala

--- sbt-plugin/src/test/scala/wartremover/InspectArgsParserTest.scala
+++ sbt-plugin/src/test/scala/wartremover/InspectArgsParserTest.scala
@@ -226,7 +226,7 @@ class InspectArgsParserTest extends AnyFunSuite with EitherValues with BeforeAnd
   }
 
   test("wart names") {
-    val v = "3.1.0"
+    val v = "3.1.1"
     val jars = coursier.Fetch().addDependencies("org.wartremover" % "wartremover_3" % v).run()
     val jar = jars.find(_.getName == s"wartremover_3-${v}.jar").getOrElse(sys.error(s"not found ${jars}"))
     val classes = WartRemover.getAllClassNamesInJar(jar)
@@ -266,7 +266,6 @@ class InspectArgsParserTest extends AnyFunSuite with EitherValues with BeforeAnd
         if (s.endsWith("$")) s.dropRight(1) else s
       }.filterNot(_.contains("$anon")).filterNot(exclude)
     } ++ Seq[String](
-      "CaseClassPrivateApply" // TODO remove when update to 3.1.1
     ).map(
       "org.wartremover.warts." + _
     )
