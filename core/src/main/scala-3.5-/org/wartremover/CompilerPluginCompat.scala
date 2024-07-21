e7e74e3a919991187dc344054fdf466f147cb0ef
xuwei-k <6b656e6a69@gmail.com>
2024-07-21 20:04:48 +0900

    fix Scala 3.5 compiler plugin warning

---
+++ core/src/main/scala-3.5-/org/wartremover/CompilerPluginCompat.scala
@@ -0,0 +1,9 @@
+package org.wartremover
+
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.plugins.PluginPhase
+
+trait CompilerPluginCompat { self: Plugin =>
+  override final def init(options: List[String]): List[PluginPhase] =
+    initializeWartremoverPlugin(options, None)
+}
