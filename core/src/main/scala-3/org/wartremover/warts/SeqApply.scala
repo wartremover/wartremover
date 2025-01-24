2531e97bca50703d3fd26952109e457a2d54c5cf
xuwei-k <6b656e6a69@gmail.com>
2025-01-25 08:01:00 +0900

    optimize SeqApply

--- core/src/main/scala-3/org/wartremover/warts/SeqApply.scala
+++ core/src/main/scala-3/org/wartremover/warts/SeqApply.scala
@@ -1,7 +1,19 @@
 package org.wartremover
 package warts
 
-object SeqApply
-    extends ExprMatch({ case '{ ($x: collection.Seq[t1]).apply($n) } =>
-      "Seq.apply is disabled"
-    })
+object SeqApply extends WartTraverser {
+  override def apply(u: WartUniverse): u.Traverser = {
+    new u.Traverser(this) {
+      import q.reflect.*
+      override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
+        tree match {
+          case _ if hasWartAnnotation(tree) =>
+          case Apply(Select(obj, "apply"), arg :: Nil) if obj.tpe <:< TypeRepr.of[collection.Seq[Any]] =>
+            error(tree.pos, "Seq.apply is disabled")
+          case _ =>
+            super.traverseTree(tree)(owner)
+        }
+      }
+    }
+  }
+}
