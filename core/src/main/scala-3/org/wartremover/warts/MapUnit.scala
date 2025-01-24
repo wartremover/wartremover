34af2d69d3d5aa9d48c4f9b7056934b7c4eed58e
xuwei-k <6b656e6a69@gmail.com>
2025-01-24 19:36:08 +0900

    optimize MapUnit

--- core/src/main/scala-3/org/wartremover/warts/MapUnit.scala
+++ core/src/main/scala-3/org/wartremover/warts/MapUnit.scala
@@ -6,18 +6,29 @@ import scala.quoted.Type
 import scala.quoted.Quotes
 import scala.quoted.quotes
 
-object MapUnit
-    extends ExprMatch({
-      import quotes.reflect.*
-
-      val f: PartialFunction[Expr[Any], String] = {
-        case '{
-              type t1
-              type t2
-              ($x: collection.Iterable[`t1`]).map($f: Function1[`t1`, `t2`])
-            } if TypeRepr.of[t2] =:= TypeRepr.of[Unit] =>
-          "Maybe you should use `foreach` instead of `map`"
+object MapUnit extends WartTraverser {
+  override def apply(u: WartUniverse): u.Traverser = {
+    new u.Traverser(this) {
+      import q.reflect.*
+      override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
+        tree match {
+          case _ if sourceCodeNotContains(tree, "map") =>
+          case _ if hasWartAnnotation(tree) =>
+          case _ if tree.isExpr =>
+            tree.asExpr match {
+              case '{
+                    type t1
+                    type t2
+                    ($x: collection.Iterable[`t1`]).map($f: Function1[`t1`, `t2`])
+                  } if TypeRepr.of[t2] =:= TypeRepr.of[Unit] =>
+                error(tree.pos, "Maybe you should use `foreach` instead of `map`")
+              case _ =>
+                super.traverseTree(tree)(owner)
+            }
+          case _ =>
+            super.traverseTree(tree)(owner)
+        }
       }
-
-      f
-    })
+    }
+  }
+}
