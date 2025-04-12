0f8f33c0a92906383f95143ae0292482ef628ed4
xuwei-k <6b656e6a69@gmail.com>
2025-04-12 20:16:12 +0900

    fix MutableDataStructures

--- core/src/main/scala-2/org/wartremover/warts/MutableDataStructures.scala
+++ core/src/main/scala-2/org/wartremover/warts/MutableDataStructures.scala
@@ -5,14 +5,12 @@ object MutableDataStructures extends WartTraverser {
   def apply(u: WartUniverse): u.Traverser = {
     import u.universe._
 
-    val mutablePackage = rootMirror.staticPackage("scala.collection.mutable")
-
     new u.Traverser {
       override def traverse(tree: Tree): Unit = {
         tree match {
           // Ignore trees marked by SuppressWarnings
           case t if hasWartAnnotation(u)(t) =>
-          case Select(tpt, _) if tpt.tpe.contains(mutablePackage) && tpt.tpe.termSymbol.isPackage =>
+          case t @ TypeTree() if t.tpe.typeSymbol.fullName.startsWith("scala.collection.mutable.") =>
             error(u)(tree.pos, "scala.collection.mutable package is disabled")
             super.traverse(tree)
           case _ => super.traverse(tree)
