9eb8786d3b2b1dde7343a368edb281b2aa922f20
kenji yoshida <6b656e6a69@gmail.com>
2025-04-19 13:52:42 +0900

    Update InspectArgsParserTest.scala

--- sbt-plugin/src/test/scala/wartremover/InspectArgsParserTest.scala
+++ sbt-plugin/src/test/scala/wartremover/InspectArgsParserTest.scala
@@ -230,7 +230,7 @@ class InspectArgsParserTest extends AnyFunSuite with EitherValues with BeforeAnd
   }
 
   test("wart names") {
-    val v = "3.3.1"
+    val v = "3.3.3"
     val jars = coursier.Fetch().addDependencies("org.wartremover" % "wartremover_3" % v).run()
     val jar = jars.find(_.getName == s"wartremover_3-${v}.jar").getOrElse(sys.error(s"not found ${jars}"))
     val classes = WartRemover.getAllClassNamesInJar(jar)
