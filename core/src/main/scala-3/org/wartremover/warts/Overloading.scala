54f12913e6e4e9262a8075618e0f337be888989f
xuwei-k <6b656e6a69@gmail.com>
2025-06-07 10:14:03 +0900

    fix Scala 3 Overloading

--- core/src/main/scala-3/org/wartremover/warts/Overloading.scala
+++ core/src/main/scala-3/org/wartremover/warts/Overloading.scala
@@ -2,6 +2,8 @@ package org.wartremover
 package warts
 
 object Overloading extends WartTraverser {
+  private final case class Sig(param: List[String | Int], result: String)
+
   def apply(u: WartUniverse): u.Traverser = {
     new u.Traverser(this) {
       import q.reflect.*
@@ -9,17 +11,34 @@ object Overloading extends WartTraverser {
         tree match {
           case t if hasWartAnnotation(t) =>
           case t: ClassDef =>
-            // TODO
-            val parentMethodNames: Set[String] = (
+            val parentMethods: Map[String, Seq[Sig]] = (
               t.parents.flatMap(_.symbol.methodMembers) ++
                 TypeRepr.of[AnyRef].classSymbol.toList.flatMap(_.methodMembers)
-            ).map(_.name).toSet
+            ).groupMap(_.name)(sym => Sig(sym.signature.paramSigs, sym.signature.resultSig))
 
-            val methods = t.body.collect { case d: DefDef => d }
+            val methods = t.body.collect {
+              case d: DefDef if d.symbol.allOverriddenSymbols.isEmpty && !d.symbol.flags.is(Flags.Synthetic) =>
+                d
+            }
             val overloads = methods
               .groupBy(_.name)
-              .map(_._2.filterNot(t => hasWartAnnotation(t)).filterNot(_.symbol.flags.is(Flags.Override)))
-              .filter(f => f.sizeIs > 1)
+              .map { (k, v) =>
+                k -> v.filterNot(t => hasWartAnnotation(t))
+              }
+              .filter {
+                case (k, Seq(v)) =>
+                  parentMethods.get(k) match {
+                    case Some(signatures) =>
+                      val s = v.symbol.signature
+                      val sig = Sig(s.paramSigs, s.resultSig)
+                      signatures.exists(_ != sig)
+                    case None =>
+                      false
+                  }
+                case _ =>
+                  true
+              }
+              .map(_._2)
 
             overloads.flatten.foreach { method =>
               error(method.pos, "Overloading is disabled")
