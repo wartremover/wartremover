c9ccc86acd253b04aa9e1c32e94f88498390d1c1
xuwei-k <6b656e6a69@gmail.com>
2025-12-06 14:08:56 +0900

    update PartialFunctionApply

--- core/src/main/scala-2/org/wartremover/warts/PartialFunctionApply.scala
+++ core/src/main/scala-2/org/wartremover/warts/PartialFunctionApply.scala
@@ -13,6 +13,11 @@ object PartialFunctionApply extends WartTraverser {
       override def traverse(tree: Tree): Unit = {
         tree match {
           case t if hasWartAnnotation(u)(t) || isSynthetic(u)(t) =>
+          case If(
+                Apply(Select(Ident(x1), TermName("isDefinedAt")), List(Ident(x2))),
+                Apply(Select(Ident(x3), TermName("apply")), List(Ident(x4))),
+                _
+              ) if (x1 == x3) && (x2 == x4) =>
           case Apply(Select(obj, TermName("apply")), _ :: Nil)
               if (obj.tpe.typeConstructor <:< pfType) && !(obj.tpe.typeConstructor <:< seqType) && !(obj.tpe.typeConstructor <:< mapType) =>
             error(u)(tree.pos, "PartialFunction#apply is disabled")
