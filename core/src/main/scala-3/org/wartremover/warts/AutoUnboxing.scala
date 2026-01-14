280157b4ca15c7044b71309d94470e6ccb3f7f65
xuwei-k <6b656e6a69@gmail.com>
2026-01-14 16:46:18 +0900

    optimize AutoUnboxing

--- core/src/main/scala-3/org/wartremover/warts/AutoUnboxing.scala
+++ core/src/main/scala-3/org/wartremover/warts/AutoUnboxing.scala
@@ -1,23 +1,32 @@
-package org.wartremover.warts
+package org.wartremover
+package warts
 
-import org.wartremover.ExprMatch
-
-object AutoUnboxing
-    extends ExprMatch({
-      case '{ Byte2byte($x) } =>
-        "java.lang.Byte to scala.Byte auto unboxing"
-      case '{ Short2short($x) } =>
-        "java.lang.Short to scala.Short auto unboxing"
-      case '{ Character2char($x) } =>
-        "java.lang.Character to scala.Char auto unboxing"
-      case '{ Integer2int($x) } =>
-        "java.lang.Integer to scala.Int auto unboxing"
-      case '{ Long2long($x) } =>
-        "java.lang.Long to scala.Long auto unboxing"
-      case '{ Float2float($x) } =>
-        "java.lang.Float to scala.Float auto unboxing"
-      case '{ Double2double($x) } =>
-        "java.lang.Double to scala.Double auto unboxing"
-      case '{ Boolean2boolean($x) } =>
-        "java.lang.Boolean to scala.Boolean auto unboxing"
-    })
+object AutoUnboxing extends WartTraverser {
+  def apply(u: WartUniverse): u.Traverser = {
+    new u.Traverser(this) {
+      import q.reflect.*
+      private val methods: Seq[(Symbol, String)] = Seq(
+        Symbol.requiredMethod("scala.Predef.Byte2byte") -> "java.lang.Byte to scala.Byte auto unboxing",
+        Symbol.requiredMethod("scala.Predef.Short2short") -> "java.lang.Short to scala.Short auto unboxing",
+        Symbol.requiredMethod("scala.Predef.Character2char") -> "java.lang.Character to scala.Char auto unboxing",
+        Symbol.requiredMethod("scala.Predef.Integer2int") -> "java.lang.Integer to scala.Int auto unboxing",
+        Symbol.requiredMethod("scala.Predef.Long2long") -> "java.lang.Long to scala.Long auto unboxing",
+        Symbol.requiredMethod("scala.Predef.Float2float") -> "java.lang.Float to scala.Float auto unboxing",
+        Symbol.requiredMethod("scala.Predef.Double2double") -> "java.lang.Double to scala.Double auto unboxing",
+        Symbol.requiredMethod("scala.Predef.Boolean2boolean") -> "java.lang.Boolean to scala.Boolean auto unboxing",
+      )
+      override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
+        tree match {
+          case t if hasWartAnnotation(t) =>
+          case Apply(method, _ :: Nil) =>
+            methods.find(_._1 == method.symbol).foreach { (_, message) =>
+              error(tree.pos, message)
+            }
+            super.traverseTree(tree)(owner)
+          case _ =>
+            super.traverseTree(tree)(owner)
+        }
+      }
+    }
+  }
+}
