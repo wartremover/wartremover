74db634115eb49514c722aaf40edc6307096a4b8
kenji yoshida <6b656e6a69@gmail.com>
2026-01-15 16:19:58 +0900

    Update Scala 3 MapContains

--- core/src/main/scala-3/org/wartremover/warts/MapContains.scala
+++ core/src/main/scala-3/org/wartremover/warts/MapContains.scala
@@ -2,11 +2,14 @@ package org.wartremover
 package warts
 
 object MapContains extends WartTraverser {
+  private val checkMethodNames = Seq("isDefined", "isEmpty", "nonEmpty")
+
   def apply(u: WartUniverse): u.Traverser = {
     new u.Traverser(this) {
       import q.reflect.*
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
+          case _ if sourceCodeNotContains(tree, "get") || checkMethodNames.forall(sourceCodeNotContains(tree, _)) =>
           case t if hasWartAnnotation(t) =>
           case Select(Apply(Select(m, "get"), Seq(arg)), "isDefined" | "isEmpty" | "nonEmpty")
               if m.tpe.derivesFrom(Symbol.requiredClass("scala.collection.Map")) =>
