ec62ae6dbcc2647316f12057c0e4a63ea91ed437
xuwei-k <6b656e6a69@gmail.com>
2026-01-15 20:53:55 +0900

    Update ThreadSleep

--- core/src/main/scala-2/org/wartremover/warts/ThreadSleep.scala
+++ core/src/main/scala-2/org/wartremover/warts/ThreadSleep.scala
@@ -9,8 +9,7 @@ object ThreadSleep extends WartTraverser {
       override def traverse(tree: Tree): Unit = {
         tree match {
           case t if hasWartAnnotation(u)(t) =>
-          case Apply(Select(obj, Sleep), List(arg))
-              if (obj.tpe.typeSymbol.fullName == "java.lang.Thread") && (arg.tpe weak_<:< typeOf[Long]) =>
+          case Apply(Select(obj, Sleep), _) if obj.tpe.typeSymbol.fullName == "java.lang.Thread" =>
             error(u)(
               tree.pos,
               "don't use Thread.sleep"
