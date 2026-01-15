06f900d3d00b89bc99deeed77a60609969148325
kenji yoshida <6b656e6a69@gmail.com>
2026-01-15 16:13:22 +0900

    Update Scala 3 FilterSize

--- core/src/main/scala-3/org/wartremover/warts/FilterSize.scala
+++ core/src/main/scala-3/org/wartremover/warts/FilterSize.scala
@@ -2,12 +2,14 @@ package org.wartremover
 package warts
 
 object FilterSize extends WartTraverser {
+  private val sizeLengthMethods = Seq("size", "length")
+
   override def apply(u: WartUniverse): u.Traverser = {
     new u.Traverser(this) {
       import q.reflect.*
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
-          case _ if sourceCodeNotContains(tree, "filter") =>
+          case _ if sourceCodeNotContains(tree, "filter") || sizeLengthMethods.forall(sourceCodeNotContains(tree, _)) =>
           case t if hasWartAnnotation(t) =>
           case t if t.isExpr =>
             t.asExpr match {
