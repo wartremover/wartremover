4ddbd1097b1cb7a70c1885442106b6e592ac4296
kenji yoshida <6b656e6a69@gmail.com>
2026-01-16 21:07:05 +0900

    optimize Scala 3 StringPlusAny

--- core/src/main/scala-3/org/wartremover/warts/StringPlusAny.scala
+++ core/src/main/scala-3/org/wartremover/warts/StringPlusAny.scala
@@ -8,18 +8,15 @@ object StringPlusAny extends WartTraverser {
   def apply(u: WartUniverse): u.Traverser = {
     new u.Traverser(this) {
       import q.reflect.*
-      object PrimitivePlusString {
-        def unapply[A](t: Expr[A]): Boolean = t match {
-          case '{ ($x1: Byte) + ($x2: String) } => true
-          case '{ ($x1: Short) + ($x2: String) } => true
-          case '{ ($x1: Char) + ($x2: String) } => true
-          case '{ ($x1: Int) + ($x2: String) } => true
-          case '{ ($x1: Long) + ($x2: String) } => true
-          case '{ ($x1: Float) + ($x2: String) } => true
-          case '{ ($x1: Double) + ($x2: String) } => true
-          case _ => false
-        }
-      }
+      val primitiveTypes: Seq[TypeRepr] = Seq(
+        TypeRepr.of[Byte],
+        TypeRepr.of[Short],
+        TypeRepr.of[Char],
+        TypeRepr.of[Int],
+        TypeRepr.of[Long],
+        TypeRepr.of[Float],
+        TypeRepr.of[Double],
+      )
 
       @nowarn("msg=any2stringadd")
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
@@ -28,13 +25,13 @@ object StringPlusAny extends WartTraverser {
           case Apply(Select(lhs, "+"), List(rhs))
               if lhs.tpe <:< TypeRepr.of[String] && !(rhs.tpe <:< TypeRepr.of[String]) =>
             error(tree.pos, "Implicit conversion to string is disabled")
+          case Apply(Select(lhs, "+"), rhs :: Nil)
+              if primitiveTypes.exists(lhs.tpe <:< _) && (rhs.tpe <:< TypeRepr.of[String]) =>
+            error(tree.pos, "Implicit conversion to string is disabled")
           case t if t.isExpr =>
             t.asExpr match {
               case '{ new Predef.any2stringadd($x) } =>
                 error(tree.pos, "Implicit conversion to string is disabled")
-              case PrimitivePlusString() =>
-                error(tree.pos, "Implicit conversion to string is disabled")
-              case '{ ($x1: String) + ($x2: String) } =>
               case _ =>
                 super.traverseTree(tree)(owner)
             }
