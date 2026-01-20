0aa2849145ba926e34af54717eeff35bb40d75f2
kenji yoshida <6b656e6a69@gmail.com>
2026-01-21 07:49:48 +0900

    Optimize Scala 3 TripleQuestionMark

--- core/src/main/scala-3/org/wartremover/warts/TripleQuestionMark.scala
+++ core/src/main/scala-3/org/wartremover/warts/TripleQuestionMark.scala
@@ -1,7 +1,19 @@
 package org.wartremover
 package warts
 
-object TripleQuestionMark
-    extends ExprMatch({ case '{ scala.Predef.??? } =>
-      "??? is disabled"
-    })
+object TripleQuestionMark extends WartTraverser {
+  override def apply(u: WartUniverse): u.Traverser = {
+    new u.Traverser(this) {
+      import q.reflect.*
+      override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
+        tree match {
+          case _ if hasWartAnnotation(tree) =>
+          case i: Ident if "scala.Predef$.???" == i.symbol.fullName =>
+            error(tree.pos, "??? is disabled")
+          case _ =>
+            super.traverseTree(tree)(owner)
+        }
+      }
+    }
+  }
+}
