fe051a4dceefcd78803693a916ff8455644bdbe5
kenji yoshida <6b656e6a69@gmail.com>
2026-01-20 20:31:14 +0900

    optimize Scala 3 GlobalExecutionContext

--- core/src/main/scala-3/org/wartremover/warts/GlobalExecutionContext.scala
+++ core/src/main/scala-3/org/wartremover/warts/GlobalExecutionContext.scala
@@ -1,10 +1,13 @@
 package org.wartremover
 package warts
 
-import scala.concurrent.ExecutionContext
-
 object GlobalExecutionContext extends WartTraverser {
   private[wartremover] def message = "Don't use ExecutionContext.global"
+
+  private val globalNames: Set[String] = Set(
+    "scala.concurrent.ExecutionContext$.Implicits$.global",
+    "scala.concurrent.ExecutionContext$.global",
+  )
 
   override def apply(u: WartUniverse): u.Traverser = {
     new u.Traverser(this) {
@@ -12,13 +15,8 @@ object GlobalExecutionContext extends WartTraverser {
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
           case t if hasWartAnnotation(t) =>
-          case t if t.isExpr =>
-            t.asExpr match {
-              case '{ ExecutionContext.global } | '{ ExecutionContext.Implicits.global } =>
-                error(tree.pos, message)
-              case _ =>
-                super.traverseTree(tree)(owner)
-            }
+          case _ if globalNames(tree.symbol.fullName) =>
+            error(tree.pos, message)
           case _ =>
             super.traverseTree(tree)(owner)
         }
