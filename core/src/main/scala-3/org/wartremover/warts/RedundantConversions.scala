6c09ecc615cbaa88d045fb6ef2bd9986f490e3d0
kenji yoshida <6b656e6a69@gmail.com>
2026-01-22 13:25:12 +0900

    Optimize Scala 3 RedundantConversions

--- core/src/main/scala-3/org/wartremover/warts/RedundantConversions.scala
+++ core/src/main/scala-3/org/wartremover/warts/RedundantConversions.scala
@@ -29,42 +29,36 @@ object RedundantConversions extends WartTraverser {
         tree match {
           case _ if methodNames.forall(sourceCodeNotContains(tree, _)) =>
           case t if hasWartAnnotation(t) =>
-          case t if t.isExpr =>
-            t.asExpr match {
-              case '{ ($x: List[?]).toList } =>
+          case Select(t, method) =>
+            method match {
+              case "toList" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.List") =>
                 error(t.pos, "redundant toList conversion")
-              case '{ ($x: collection.immutable.Seq[?]).toSeq } =>
+              case "toSeq" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Seq") =>
                 error(t.pos, "redundant toSeq conversion")
-              case '{ ($x: Vector[?]).toVector } =>
+              case "toVector" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Vector") =>
                 error(t.pos, "redundant toVector conversion")
-              case '{ ($x: Stream[?]).toStream } =>
+              case "toStream" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Stream") =>
                 error(t.pos, "redundant toStream conversion")
-              case '{
-                    type t1
-                    type t2 >: `t1`
-                    ($x: Set[`t1`]).toSet[`t2`]
-                  } =>
-                // note https://github.com/scala/scala3/issues/14708
+              case "toSet" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.Set") =>
                 error(t.pos, "redundant toSet conversion")
-              case '{ ($x: collection.immutable.IndexedSeq[?]).toIndexedSeq } =>
+              case "toIndexedSeq" if t.tpe.baseClasses.exists(_.fullName == "scala.collection.immutable.IndexedSeq") =>
                 error(t.pos, "redundant toIndexedSeq conversion")
-              case '{ ($x: String).toString } =>
+              case "toString" if t.tpe <:< TypeRepr.of[String] =>
                 error(t.pos, "redundant toString conversion")
-              case '{ ($x: Int).toInt } =>
+              case "toInt" if t.tpe <:< TypeRepr.of[Int] =>
                 error(t.pos, "redundant toInt conversion")
-              case '{ ($x: Long).toLong } =>
+              case "toLong" if t.tpe <:< TypeRepr.of[Long] =>
                 error(t.pos, "redundant toLong conversion")
-              case '{ ($x: Float).toFloat } =>
+              case "toFloat" if t.tpe <:< TypeRepr.of[Float] =>
                 error(t.pos, "redundant toFloat conversion")
-              case '{ ($x: Double).toDouble } =>
+              case "toDouble" if t.tpe <:< TypeRepr.of[Double] =>
                 error(t.pos, "redundant toDouble conversion")
-              case '{ ($x: Byte).toByte } =>
+              case "toByte" if t.tpe <:< TypeRepr.of[Byte] =>
                 error(t.pos, "redundant toByte conversion")
-              case '{ ($x: Short).toShort } =>
+              case "toShort" if t.tpe <:< TypeRepr.of[Short] =>
                 error(t.pos, "redundant toShort conversion")
-              case '{ ($x: Char).toChar } =>
+              case "toChar" if t.tpe <:< TypeRepr.of[Char] =>
                 error(t.pos, "redundant toChar conversion")
-
               case _ =>
                 super.traverseTree(tree)(owner)
             }
