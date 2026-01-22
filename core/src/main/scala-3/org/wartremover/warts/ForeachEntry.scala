b4baac21c5be7b9bfa85ae66cb489c17ebcf2d28
kenji yoshida <6b656e6a69@gmail.com>
2026-01-22 11:20:25 +0900

    Optimize Scala 3 ForeachEntry

--- core/src/main/scala-3/org/wartremover/warts/ForeachEntry.scala
+++ core/src/main/scala-3/org/wartremover/warts/ForeachEntry.scala
@@ -9,41 +9,31 @@ object ForeachEntry extends WartTraverser {
         tree match {
           case _ if sourceCodeNotContains(tree, "for") =>
           case t if hasWartAnnotation(t) =>
-          case t if t.isExpr =>
-            t.asExpr match {
-              case '{ ($x: collection.Map[t1, t2]).foreach($f) } =>
-                f.asTerm match {
-                  case b @ Block(
-                        List(
-                          DefDef(
-                            _,
-                            _,
-                            _,
-                            Some(
-                              Match(
-                                _,
-                                cases
-                              )
-                            )
-                          )
-                        ),
-                        _: Closure
-                      ) =>
-                    if (
-                      cases.nonEmpty && cases.forall {
-                        case CaseDef(Unapply(TypeApply(Select(Ident("Tuple2"), "unapply"), _), _, _), _, _) =>
-                          true
-                        case _ =>
-                          false
-                      }
-                    ) {
-                      error(tree.pos, "You can use `foreachEntry` instead of `foreach` if Scala 2.13+")
-                    }
-                  case _ =>
-                }
-              case _ =>
-            }
-            super.traverseTree(tree)(owner)
+          case Apply(
+                TypeApply(Select(x, "foreach"), _),
+                Block(
+                  List(
+                    DefDef(
+                      _,
+                      _,
+                      _,
+                      Some(
+                        Match(
+                          _,
+                          cases
+                        )
+                      )
+                    )
+                  ),
+                  _: Closure
+                ) :: Nil
+              ) if cases.nonEmpty && cases.forall {
+                case CaseDef(Unapply(TypeApply(Select(Ident("Tuple2"), "unapply"), _), _, _), _, _) =>
+                  true
+                case _ =>
+                  false
+              } && x.tpe.baseClasses.exists(_.fullName == "scala.collection.Map") =>
+            error(tree.pos, "You can use `foreachEntry` instead of `foreach` if Scala 2.13+")
           case _ =>
             super.traverseTree(tree)(owner)
         }
