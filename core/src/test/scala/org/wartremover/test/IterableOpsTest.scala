966c0b1fa663cb5c73ac367a747f0d0a995a3c6c
kenji yoshida <6b656e6a69@gmail.com>
2026-01-22 16:54:36 +0900

    fix IterableOpsTest

--- core/src/test/scala/org/wartremover/test/IterableOpsTest.scala
+++ core/src/test/scala/org/wartremover/test/IterableOpsTest.scala
@@ -6,11 +6,92 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class IterableOpsTest extends AnyFunSuite with ResultAssertions {
 
-  implicit val ordering: Ordering[Any] = new Ordering[Any] {
-    override def compare(x: Any, y: Any) = 0
+  locally {
+    def name = "List"
+    def x: List[Int] = Nil
+
+    test(s"can't use $name#head") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.head)
+      }
+      assertError(result)("head is disabled - use headOption instead")
+    }
+
+    test(s"can't use $name#tail") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.tail)
+      }
+      assertError(result)("tail is disabled - use drop(1) instead")
+    }
+
+    test(s"can't use $name#init") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.init)
+      }
+      assertError(result)("init is disabled - use dropRight(1) instead")
+    }
+
+    test(s"can't use $name#last") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.last)
+      }
+      assertError(result)("last is disabled - use lastOption instead")
+    }
+
+    test(s"can't use $name#reduce") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.reduce(_.hashCode + _.hashCode))
+      }
+      assertError(result)("reduce is disabled - use reduceOption or fold instead")
+    }
+
+    test(s"can't use $name#reduceLeft") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.reduceLeft(_.hashCode + _.hashCode))
+      }
+      assertError(result)("reduceLeft is disabled - use reduceLeftOption or foldLeft instead")
+    }
+
+    test(s"can't use $name#reduceRight") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.reduceRight(_.hashCode + _.hashCode))
+      }
+      assertError(result)("reduceRight is disabled - use reduceRightOption or foldRight instead")
+    }
+
+    test(s"can't use $name#max") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.max)
+      }
+      assertError(result)("max is disabled - use foldLeft or foldRight instead")
+    }
+
+    test(s"can't use $name#maxBy") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.maxBy(_.hashCode))
+      }
+      assertError(result)("maxBy is disabled - use foldLeft or foldRight instead")
+    }
+
+    test(s"can't use $name#min") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.min)
+      }
+      assertError(result)("min is disabled - use foldLeft or foldRight instead")
+    }
+
+    test(s"can't use $name#minBy") {
+      val result = WartTestTraverser(IterableOps) {
+        println(x.minBy(_.hashCode))
+      }
+      assertError(result)("minBy is disabled - use foldLeft or foldRight instead")
+    }
   }
 
-  Seq[(String, Iterable[Any])]("List" -> List(1), "Seq" -> Seq(1), "Map" -> Map(1 -> 1)).foreach { case (name, x) =>
+  locally {
+    def name = "Set"
+    def x: Set[Int] = Set.empty
+
     test(s"can't use $name#head") {
       val result = WartTestTraverser(IterableOps) {
         println(x.head)
