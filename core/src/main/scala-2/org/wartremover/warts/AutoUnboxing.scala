ccfa88ca912c88968e2e371555d89749bd7053b0
xuwei-k <6b656e6a69@gmail.com>
2026-01-29 18:58:41 +0900

    Update Scala 2 AutoUnboxing

--- core/src/main/scala-2/org/wartremover/warts/AutoUnboxing.scala
+++ core/src/main/scala-2/org/wartremover/warts/AutoUnboxing.scala
@@ -10,22 +10,27 @@ object AutoUnboxing extends WartTraverser {
       override def traverse(tree: Tree): Unit = {
         tree match {
           case _ if hasWartAnnotation(u)(tree) =>
-          case q"scala.Predef.Byte2byte($x)" =>
-            error(u)(tree.pos, "java.lang.Byte to scala.Byte auto unboxing")
-          case q"scala.Predef.Short2short($x)" =>
-            error(u)(tree.pos, "java.lang.Short to scala.Short auto unboxing")
-          case q"scala.Predef.Character2char($x)" =>
-            error(u)(tree.pos, "java.lang.Character to scala.Char auto unboxing")
-          case q"scala.Predef.Integer2int($x)" =>
-            error(u)(tree.pos, "java.lang.Integer to scala.Int auto unboxing")
-          case q"scala.Predef.Long2long($x)" =>
-            error(u)(tree.pos, "java.lang.Long to scala.Long auto unboxing")
-          case q"scala.Predef.Float2float($x)" =>
-            error(u)(tree.pos, "java.lang.Float to scala.Float auto unboxing")
-          case q"scala.Predef.Double2double($x)" =>
-            error(u)(tree.pos, "java.lang.Double to scala.Double auto unboxing")
-          case q"scala.Predef.Boolean2boolean($x)" =>
-            error(u)(tree.pos, "java.lang.Boolean to scala.Boolean auto unboxing")
+          case Apply(method, _ :: Nil) =>
+            method.symbol.fullName match {
+              case "scala.Predef.Byte2byte" =>
+                error(u)(tree.pos, "java.lang.Byte to scala.Byte auto unboxing")
+              case "scala.Predef.Short2short" =>
+                error(u)(tree.pos, "java.lang.Short to scala.Short auto unboxing")
+              case "scala.Predef.Character2char" =>
+                error(u)(tree.pos, "java.lang.Character to scala.Char auto unboxing")
+              case "scala.Predef.Integer2int" =>
+                error(u)(tree.pos, "java.lang.Integer to scala.Int auto unboxing")
+              case "scala.Predef.Long2long" =>
+                error(u)(tree.pos, "java.lang.Long to scala.Long auto unboxing")
+              case "scala.Predef.Float2float" =>
+                error(u)(tree.pos, "java.lang.Float to scala.Float auto unboxing")
+              case "scala.Predef.Double2double" =>
+                error(u)(tree.pos, "java.lang.Double to scala.Double auto unboxing")
+              case "scala.Predef.Boolean2boolean" =>
+                error(u)(tree.pos, "java.lang.Boolean to scala.Boolean auto unboxing")
+              case _ =>
+                super.traverse(tree)
+            }
           case _ =>
             super.traverse(tree)
         }
