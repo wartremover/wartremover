8581d8a93483750ab9580b310a1c8af3d3de1dee
xuwei-k <6b656e6a69@gmail.com>
2026-02-01 20:39:57 +0900

    Update ArrayEquals

--- core/src/main/scala-2/org/wartremover/warts/ArrayEquals.scala
+++ core/src/main/scala-2/org/wartremover/warts/ArrayEquals.scala
@@ -7,22 +7,32 @@ object ArrayEquals extends WartTraverser {
   def apply(u: WartUniverse): u.Traverser = {
     import u.universe._
 
-    val Equals = TermName(NameTransformer.encode("=="))
+    val Equals = "equals"
+    val EqualsSymbol = NameTransformer.encode("==")
+    val NotEqualsSymbol = NameTransformer.encode("!=")
 
     new u.Traverser {
       override def traverse(tree: Tree): Unit = {
         tree match {
           // Ignore trees marked by SuppressWarnings
           case t if hasWartAnnotation(u)(t) =>
-          case Apply(Select(array, Equals), Seq(Literal(Constant(null)))) =>
+          case Apply(Select(array, TermName(EqualsSymbol | NotEqualsSymbol)), Seq(Literal(Constant(null)))) =>
             // https://github.com/wartremover/wartremover/issues/448
             super.traverse(tree)
-          case Apply(Select(array, Equals), _)
+          case Apply(Select(array, TermName(op)), _)
               if (array.tpe.typeSymbol.fullName == "scala.Array" || array.tpe <:< typeOf[
                 Iterator[Any]
               ]) && !isSynthetic(u)(tree) =>
-            error(u)(tree.pos, "== is disabled")
-            super.traverse(tree)
+            op match {
+              case Equals =>
+                error(u)(tree.pos, "equals is disabled")
+              case EqualsSymbol =>
+                error(u)(tree.pos, "== is disabled")
+              case NotEqualsSymbol =>
+                error(u)(tree.pos, "!= is disabled")
+              case _ =>
+                super.traverse(tree)
+            }
           case _ =>
             super.traverse(tree)
         }
