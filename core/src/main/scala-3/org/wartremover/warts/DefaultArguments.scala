64114ecb0927f99a072e628924bf8e740adf5a01
kenji yoshida <6b656e6a69@gmail.com>
2026-02-03 12:55:39 +0900

    Updae Scala 3 DefaultArguments position

--- core/src/main/scala-3/org/wartremover/warts/DefaultArguments.scala
+++ core/src/main/scala-3/org/wartremover/warts/DefaultArguments.scala
@@ -8,15 +8,18 @@ object DefaultArguments extends WartTraverser {
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
           case t if hasWartAnnotation(t) =>
-          case t: DefDef
-              if !t.symbol.flags.is(Flags.Synthetic) &&
-                t.termParamss
-                  .flatMap(_.params)
-                  .exists(p =>
-                    p.symbol.flags.is(Flags.HasDefault) &&
-                      !p.symbol.flags.is(Flags.Synthetic)
-                  ) =>
-            error(tree.pos, "Function has default arguments")
+          case t: DefDef if !t.symbol.flags.is(Flags.Synthetic) =>
+            t.termParamss
+              .flatMap(_.params)
+              .find(p =>
+                p.symbol.flags.is(Flags.HasDefault) &&
+                  !p.symbol.flags.is(Flags.Synthetic)
+              ) match {
+              case Some(p) =>
+                error(p.pos, "Function has default arguments")
+              case _ =>
+                super.traverseTree(tree)(owner)
+            }
           case _ =>
             super.traverseTree(tree)(owner)
         }
