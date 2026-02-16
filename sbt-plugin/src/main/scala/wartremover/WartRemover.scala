beb86958fefffbb1933c4bdf9c271194d4201521
xuwei-k <6b656e6a69@gmail.com>
2026-02-16 19:11:33 +0900

    Fix console/scalacOptions

--- sbt-plugin/src/main/scala/wartremover/WartRemover.scala
+++ sbt-plugin/src/main/scala/wartremover/WartRemover.scala
@@ -578,18 +578,20 @@ object WartRemover extends sbt.AutoPlugin with WartRemoverCompat {
         )
       )
     ),
-    Seq(Compile, Test).map(x =>
-      x / compile / scalacOptions := {
-        // https://github.com/wartremover/wartremover/issues/1233
-        val old = (x / compile / scalacOptions).value
-        val (wartremoverOptions, other) = old.partition(_.startsWith("-P:wartremover"))
-        val distinctValues = wartremoverOptions.distinct
-        if (distinctValues == wartremoverOptions) {
-          old
-        } else {
-          distinctValues ++ other
+    Seq(Compile, Test).flatMap(x =>
+      Seq(compile, console).map(y =>
+        x / y / scalacOptions := {
+          // https://github.com/wartremover/wartremover/issues/1233
+          val old = (x / y / scalacOptions).value
+          val (wartremoverOptions, other) = old.partition(_.startsWith("-P:wartremover"))
+          val distinctValues = wartremoverOptions.distinct
+          if (distinctValues == wartremoverOptions) {
+            old
+          } else {
+            distinctValues ++ other
+          }
         }
-      }
+      )
     ),
   )
 
