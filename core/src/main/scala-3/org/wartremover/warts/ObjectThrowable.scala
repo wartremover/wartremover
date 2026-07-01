86fbd1cd78f647f3f45c88d09dab4f013953dc08
xuwei-k <6b656e6a69@gmail.com>
2026-07-01 15:34:37 +0900

    Support enum in ObjectThrowable

--- core/src/main/scala-3/org/wartremover/warts/ObjectThrowable.scala
+++ core/src/main/scala-3/org/wartremover/warts/ObjectThrowable.scala
@@ -7,7 +7,7 @@ object ObjectThrowable extends WartTraverser {
       import q.reflect.*
       override def traverseTree(tree: Tree)(owner: Symbol): Unit = {
         tree match {
-          case _ if sourceCodeNotContains(tree, "object") =>
+          case _ if sourceCodeNotContains(tree, "object") && sourceCodeNotContains(tree, "case") =>
           case t if hasWartAnnotation(t) =>
           case t: ClassDef if t.symbol.flags.is(Flags.Module) =>
             val types = t.parents.collect {
@@ -22,6 +22,13 @@ object ObjectThrowable extends WartTraverser {
             ) {
               error(t.pos, "use class if extends Throwable")
             }
+            super.traverseTree(tree)(owner)
+          case t: ValDef
+              if t.symbol.flags.is(Flags.Case) &&
+                t.symbol.flags.is(Flags.Enum) &&
+                (t.tpt.tpe <:< TypeRepr.of[java.lang.Throwable]) &&
+                !(t.tpt.tpe <:< TypeRepr.of[scala.util.control.NoStackTrace]) =>
+            error(t.pos, "Don't use empty param enum case if extends Throwable")
             super.traverseTree(tree)(owner)
           case _ =>
             super.traverseTree(tree)(owner)
